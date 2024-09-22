8589823d707afeae4f1ec246ba5cdb15435649b9
Refilwe <refilwemotseo2@gmail.com>
2024-09-22 11:33:49 +0200

    feat: added NoQuestionsAdded component

---
+++ ai-chatbot-frontend/src/components/NoQuestionsAdded.jsx
@@ -0,0 +1,21 @@
+import Text from './Text';
+import chatBubbles from '../assets/chat-bubbles.png';
+
+const NoQuestionsAdded = () => {
+    return (
+        <div className='no-questions-added shadow'>
+            <Text text={'Search History'} fontWeight={600} fontSize={14} />
+            <div className='flex flex-col items-center gap-3'>
+                <div className="chat-bubbles">
+                    <img src={chatBubbles} alt="chat-bubble" width={'100%'} />
+                </div>
+                <div className='flex flex-col items-center text'>
+                    <Text text={'No Questions added'} fontWeight={600} fontSize={18} />
+                    <Text text={'Type your questions to below input and get fast answers'} fontWeight={400} fontSize={14} color='#727676' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NoQuestionsAdded;
