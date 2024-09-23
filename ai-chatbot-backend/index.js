67a812a045cc4b186ee30d6134feff9d7505a582
Refilwe <refilwemotseo2@gmail.com>
2024-09-23 09:21:52 +0200

    feat: added server entry file

---
+++ ai-chatbot-backend/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+require('./db');
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({ origin: '*' }));
+app.use(express.json({ limit: '1MB' }));
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port:${PORT}`);
+});
