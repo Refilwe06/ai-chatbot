75d699fda9fd4181b5c729517ed06fcfc36c247a
Refilwe <refilwemotseo2@gmail.com>
2024-09-23 18:54:11 +0200

    feat: created login API

--- ai-chatbot-backend/routes/auth.js
+++ ai-chatbot-backend/routes/auth.js
@@ -26,4 +26,23 @@ router.post('/register', async (req, res) => {
     }
 })
 
+router.post('/login', async (req, res) => {
+    const { email, user_password } = req.body;
+    try {
+        const [[dbUser], _fields] = (await db.query(`SELECT * FROM users WHERE email = '${email}'`));
+        if (!dbUser) return res.status(409).send({ err: 'User does not exist' });
+        else {
+            if (await bcrypt.compare(user_password, dbUser.user_password)) {
+                delete dbUser.user_password;
+                const token = sign({ email }, process.env.JWT_SECRET, { expiresIn: '1d' });
+                return res.send({ ...dbUser, token });
+            }
+            return res.status(401).json({ err: 'Invalid Credentials' })
+        }
+    } catch (err) {
+        console.error(err);
+        res.send(err);
+    }
+})
+
 module.exports = router;
