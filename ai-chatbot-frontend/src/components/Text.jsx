94ddf9e857d379c4c84d707030d0d0dc0f67a731
Refilwe <refilwemotseo2@gmail.com>
2024-09-26 22:47:32 +0200

    styles: removed default color to avoid overriding with !important

--- ai-chatbot-frontend/src/components/Text.jsx
+++ ai-chatbot-frontend/src/components/Text.jsx
@@ -1,4 +1,4 @@
-const Text = ({ text, fontSize = 16, fontWeight = 'normal', color = '#000', textDecoration = 'none', textTransform, className, action }) => {
+const Text = ({ text, fontSize = 16, fontWeight = 'normal', color = '', textDecoration = 'none', textTransform, className, action }) => {
     return <span onClick={action} style={{ fontSize, fontWeight, color, textDecoration, textTransform }} className={className}>{text}</span>
 }
 
