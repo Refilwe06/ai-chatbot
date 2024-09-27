91168e2c6da229b9758fd84dc3303671f11b3228
Refilwe <refilwemotseo2@gmail.com>
2024-09-28 01:18:28 +0200

    fix: fixed asked questions count and time elapsed for date created

--- ai-chatbot-frontend/src/components/ChatCard.jsx
+++ ai-chatbot-frontend/src/components/ChatCard.jsx
@@ -2,8 +2,15 @@ import chatIcon from '../assets/chat-icon.png';
 import avatar from '../assets/avatar.png';
 import Text from './Text';
 import { timeLapsed } from '../utilities/setElapsedTime';
+import { useEffect, useState } from 'react';
 
 const ChatCard = ({ width, chatHistory, onClick }) => {
+    const [chat_created_at, setCreatedAt] = useState('');
+    useEffect(() => {
+        setTimeout(() => {
+            setCreatedAt(timeLapsed(chatHistory.created_at))
+        }, 1000);
+    })
 
     return (
         <div className="flex chat-card pointer" style={{ width }} onClick={() => onClick()}>
@@ -13,7 +20,7 @@ const ChatCard = ({ width, chatHistory, onClick }) => {
             </div>
             <div className="flex flex-col text ellipsis">
                 <Text text={chatHistory.first_question} fontSize={16} fontWeight='600' style={{ width: '80%' }} className={'ellipsis'} />
-                <Text text={`${chatHistory.question_history.length / 2} ${chatHistory.question_history.length / 2 > 1 ? 'Questions' : 'Question'} asked • ${timeLapsed(chatHistory.created_at)}`} fontSize={14} fontWeight='500' color='#727676' />
+                <Text text={`${chatHistory.question_history.length / 2} ${chatHistory.question_history.length / 2 > 1 ? 'Questions' : 'Question'} asked • ${chat_created_at}`} fontSize={14} fontWeight='500' color='#727676' />
             </div>
         </div>
     )
