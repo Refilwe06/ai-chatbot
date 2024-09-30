36a752fbee259495c96bb45038e030e056fc4bdf
Refilwe <refilwemotseo2@gmail.com>
2024-09-30 02:03:01 +0200

    refactor: prevent app access from users without tokens or being logged in

--- ai-chatbot-frontend/src/context/UserContext.js
+++ ai-chatbot-frontend/src/context/UserContext.js
@@ -3,7 +3,9 @@ import { createContext, useState } from "react";
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+    const localUserString = localStorage.getItem('user');
+    const localUser = (localUserString && localUserString !== 'undefined') ? JSON.parse(localStorage.getItem('user')) : null;
+    const [user, setUser] = useState(localUser);
 
     return <UserContext.Provider value={{ user, setUser }}>
         {children}
--- ai-chatbot-frontend/src/utilities/ProtectedRoutes.js
+++ ai-chatbot-frontend/src/utilities/ProtectedRoutes.js
@@ -3,8 +3,17 @@ import { UserContext } from '../context/UserContext'
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoutes = () => {
-    const { user } = useContext(UserContext);
-    return user ? <Outlet /> : <Navigate to='/login' />
+    const { user, setUser } = useContext(UserContext);
+    const token = localStorage.getItem('token');
+
+    if (!token || !user) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        setUser(null);
+        return <Navigate to='/login' />
+    }
+
+    return <Outlet />
 }
 
 export default ProtectedRoutes;
